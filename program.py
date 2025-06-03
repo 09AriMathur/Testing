714946246fb6302e4dac7667f847661c47131bda
Aryaman Mathur <140009698+09AriMathur@users.noreply.github.com>
2025-06-03 14:36:57 +0530

    Update program.py

--- program.py
+++ program.py
@@ -2,10 +2,5 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
-data = pd.DataFrame(
-    np.random.randn(10, 2),
-    columns=["A", "B"]
-)
-
-st.line_chart(data)
+st.title("Hello, Streamlit!")
 
